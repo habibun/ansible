4fd05ca66eaedfe691ca18901a1e0fe2312c6d18
Habibun Noby <habibun.noby@gmail.com>
2021-01-21 21:36:13 +0600

    Add hosts

---
+++ hosts/cmd.sh
@@ -0,0 +1,7 @@
+ ansible 127.0.0.1 -m ping -i hosts/hosts.ini
+
+ ansible local -m ping -i hosts/hosts.ini
+
+ ansible local --list-hosts -i hosts/hosts.ini
+
+ ansible vb -m ping -i hosts/hosts.ini
